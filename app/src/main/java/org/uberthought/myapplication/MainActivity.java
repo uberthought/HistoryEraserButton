6f4fc4b9ba4148f08b89e6e258b9b80d85c9a8f5
Phil Richardson <prichardson@aumansoftware.com>
2016-09-25 17:31:25 -0400

    started moving list code to a fragment

--- app/src/main/java/org/uberthought/myapplication/MainActivity.java
+++ app/src/main/java/org/uberthought/myapplication/MainActivity.java
@@ -1,21 +1,14 @@
 package org.uberthought.myapplication;
 
-import android.database.Cursor;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import com.j256.ormlite.android.apptools.OpenHelperManager;
 import com.j256.ormlite.dao.Dao;
 
 import java.sql.SQLException;
 import java.util.Date;
-import java.util.List;
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,53 +19,53 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView listView = (ListView) findViewById(R.id.listView);
+//        ListView listView = (ListView) findViewById(R.id.listView);
+//
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                try {
+//                    Cursor cursor = getDatabaseHelper().getSimpleRecordCursor();
+//
+//                    cursor.move(i);
+//                    final Long id = cursor.getLong(cursor.getColumnIndex("_id"));
+//
+//                    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(adapterView.getContext());
+//                    alertDialogBuilder
+//                            .setMessage("Click Delete to remove record")
+//                            .setCancelable(true)
+//                            .setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//                                @Override
+//                                public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+//                                    try {
+//                                        Dao<SimpleRecord, Long> dao = getDatabaseHelper().getDao();
+//                                        dao.deleteById(id);
+//                                    } catch (SQLException e) {
+//                                        e.printStackTrace();
+//                                    }
+//                                }
+//
+//                                @Override
+//                                public void onNothingSelected(AdapterView<?> adapterView) {
+//
+//                                }
+//                            });
+//                    AlertDialog alertDialog = alertDialogBuilder.create();
+//                    alertDialog.show();
+//
+//                } catch (SQLException e) {
+//                    e.printStackTrace();
+//                }
+//
+//                UpdateEntryCount();
+//                UpdateListView();
+//
+//            }
+//        });
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                try {
-                    Cursor cursor = getDatabaseHelper().getSimpleRecordCursor();
-
-                    cursor.move(i);
-                    final Long id = cursor.getLong(cursor.getColumnIndex("_id"));
-
-                    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(adapterView.getContext());
-                    alertDialogBuilder
-                            .setMessage("Click Delete to remove record")
-                            .setCancelable(true)
-                            .setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                                @Override
-                                public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                                    try {
-                                        Dao<SimpleRecord, Long> dao = getDatabaseHelper().getDao();
-                                        dao.deleteById(id);
-                                    } catch (SQLException e) {
-                                        e.printStackTrace();
-                                    }
-                                }
-
-                                @Override
-                                public void onNothingSelected(AdapterView<?> adapterView) {
-
-                                }
-                            });
-                    AlertDialog alertDialog = alertDialogBuilder.create();
-                    alertDialog.show();
-
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-
-                UpdateEntryCount();
-                UpdateListView();
-
-            }
-        });
-
-        UpdateEntryCount();
-        UpdateListView();
+//        UpdateEntryCount();
+//        UpdateListView();
     }
 
 
@@ -87,8 +80,8 @@ public class MainActivity extends AppCompatActivity {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        UpdateEntryCount();
-        UpdateListView();
+//        UpdateEntryCount();
+//        UpdateListView();
     }
 
     public void deleteLastOnClick(View view) {
@@ -99,37 +92,37 @@ public class MainActivity extends AppCompatActivity {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        UpdateEntryCount();
-        UpdateListView();
+//        UpdateEntryCount();
+//        UpdateListView();
     }
 
-    void UpdateListView()
-    {
-        try {
-            Cursor cursor = getDatabaseHelper().getSimpleRecordCursor();
-
-            ListView listView = (ListView)findViewById(R.id.listView);
-            SimpleRecordCursorAdapter adapter = new SimpleRecordCursorAdapter(this, cursor);
-            listView.setAdapter(adapter);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void UpdateEntryCount() {
-        TextView textView = (TextView)findViewById(R.id.textView);
-        textView.setText(String.format(Locale.US, "%d", this.entryCount()));
-    }
+//    void UpdateListView()
+//    {
+//        try {
+//            Cursor cursor = getDatabaseHelper().getSimpleRecordCursor();
+//
+//            ListView listView = (ListView)findViewById(R.id.listView);
+//            SimpleRecordCursorAdapter adapter = new SimpleRecordCursorAdapter(this, cursor);
+//            listView.setAdapter(adapter);
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    private void UpdateEntryCount() {
+//        TextView textView = (TextView)findViewById(R.id.textView);
+//        textView.setText(String.format(Locale.US, "%d", this.entryCount()));
+//    }
 
     int entryCount() {
-        try {
-            List<SimpleRecord> simpleRecords = getDatabaseHelper().getAllSimpleRecords();
-            return simpleRecords.size();
-
-        } catch (SQLException e) {
-            e.printStackTrace();
+//        try {
+//            List<SimpleRecord> simpleRecords = getDatabaseHelper().getAllSimpleRecords();
+//            return simpleRecords.size();
+//
+//        } catch (SQLException e) {
+//            e.printStackTrace();
             return 0;
-        }
+//        }
     }
 
     MainDBHelper getDatabaseHelper() {
