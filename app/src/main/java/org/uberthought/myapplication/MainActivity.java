cd0a9c69ba52c5e8a073c6b6466f2953b1d283f8
Phil Richardson <prichardson@aumansoftware.com>
2016-10-28 10:07:24 -0400

    added count of tracked item events to each tracked event displayed started adding voice recognition

--- app/src/main/java/org/uberthought/myapplication/MainActivity.java
+++ app/src/main/java/org/uberthought/myapplication/MainActivity.java
@@ -1,11 +1,18 @@
 package org.uberthought.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.j256.ormlite.android.apptools.OpenHelperManager;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
@@ -39,8 +46,23 @@ public class MainActivity extends AppCompatActivity {
         getDatabaseHelper().addDBListener(this::onDatabaseChange);
 
         onDatabaseChange();
+
+        Button speechInputButton = (Button)findViewById(R.id.speech_input);
+        speechInputButton.setOnClickListener(v -> {
+            Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+            intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speech recognition demo");
+            startActivityForResult(intent, 1234);
+        });
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1234) {
+            ArrayList<String> matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            Toast.makeText(this, matches.get(0), Toast.LENGTH_LONG).show();
+        }
+    }
 
     private void onDatabaseChange() {
         TextView textView = (TextView) findViewById(R.id.textView);
@@ -52,4 +74,6 @@ public class MainActivity extends AppCompatActivity {
             databaseHelper = OpenHelperManager.getHelper(this, MainDBHelper.class);
         return databaseHelper;
     }
+
+
 }
--- app/src/main/java/org/uberthought/myapplication/TrackedItemCursorAdapter.java
+++ app/src/main/java/org/uberthought/myapplication/TrackedItemCursorAdapter.java
@@ -7,7 +7,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.j256.ormlite.android.apptools.OpenHelperManager;
+
+import java.sql.SQLException;
+
 class TrackedItemCursorAdapter extends BaseCursorAdapter {
+
+    private MainDBHelper databaseHelper;
 
     TrackedItemCursorAdapter(Context context, Cursor c) {
         super(context, c);
@@ -22,11 +28,28 @@ class TrackedItemCursorAdapter extends BaseCursorAdapter {
     public void bindView(View view, Context context, Cursor cursor) {
 
         String name = cursor.getString(cursor.getColumnIndex("name"));
+        long id = cursor.getLong(cursor.getColumnIndex("_id"));
+
+        MainDBHelper mainDBHelper = getDatabaseHelper(context);
+
+        int count = 0;
+        try {
+            count = mainDBHelper.getSimpleRecordCursor(id).getCount();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
         TextView textView = (TextView) view.findViewById((R.id.textView2));
-        textView.setText(name);
+        textView.setText(String.format("%s %d", name, count));
 
         CheckableLinearLayout checkableLinearLayout = (CheckableLinearLayout) view;
         checkableLinearLayout.setCheckable(isCheckable());
     }
+
+    private MainDBHelper getDatabaseHelper(Context context) {
+        if (databaseHelper == null)
+            databaseHelper = OpenHelperManager.getHelper(context, MainDBHelper.class);
+        return databaseHelper;
+    }
+
 }
