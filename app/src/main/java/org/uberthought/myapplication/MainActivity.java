d019f2c22121e1ece61d083f50aa0a88a5176b71
Philip Richardson <prichardson@aumansoftware.com>
2017-05-05 17:28:32 -0400

    move login button to it's own fragment

--- app/src/main/java/org/uberthought/myapplication/MainActivity.java
+++ app/src/main/java/org/uberthought/myapplication/MainActivity.java
@@ -3,8 +3,8 @@ package org.uberthought.myapplication;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -13,17 +13,30 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
     private static final String TAG = "MainActivity";
     GoogleApiClient mGoogleApiClient;
-    Button signOutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        String[] drawerTitles = new String[]{"Log Out"};
+        ListView drawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // Set the adapter for the list view
+        drawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, drawerTitles));
+
+        // Set the list's click listener
+        drawerList.setOnItemClickListener((parent, view, position, id) -> {
+            if (position == 0) {
+                Auth.GoogleSignInApi.signOut(mGoogleApiClient);
+                FirebaseAuth.getInstance().signOut();
+            }
+        });
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
@@ -33,9 +46,6 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
                 .enableAutoManage(this, this)
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
-
-        signOutButton = (Button) findViewById(R.id.sign_out);
-        signOutButton.setOnClickListener(this);
 
         FirebaseAuth.getInstance().addAuthStateListener(firebaseAuth -> {
             FirebaseUser user = firebaseAuth.getCurrentUser();
@@ -52,14 +62,6 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
                         .replace(R.id.FragmentView, TrackedItemFragment.newInstance())
                         .commit();
         });
-    }
-
-    @Override
-    public void onClick(View view) {
-        if (view.getId() == R.id.sign_out) {
-            Auth.GoogleSignInApi.signOut(mGoogleApiClient);
-            FirebaseAuth.getInstance().signOut();
-        }
     }
 
     @Override
